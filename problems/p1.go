b35abcbd8c45f272c82853221931ce2ffa468618
andras <andras.csibi@gmail.com>
2013-09-10 01:23:32 +0200

    solved problem 2, some cli scaffolding

--- problems/p1.go
+++ problems/p1.go
@@ -1,4 +1,10 @@
-package problems
+package main
+
+import (
+	"fmt"
+	"os"
+	"strconv"
+)
 
 func P1(n int) int {
 	sum := 0
@@ -8,4 +14,37 @@ func P1(n int) int {
 		}
 	}
 	return sum
+}
+
+func P2(n int) int {
+	sum := 0
+	for a, b := 1, 2; b < n; a, b = b, a+b {
+		if b%2 == 0 {
+			sum += b
+		}
+	}
+	return sum
+}
+
+type Problem struct {
+	solution func(int) int
+	n        int
+}
+
+var problems = map[int]Problem{
+	1: {P1, 1000},
+	2: {P2, 4e6},
+}
+
+func main() {
+	problemNumber, _ := strconv.Atoi(os.Args[1])
+	problem, found := problems[problemNumber]
+	if !found {
+		fmt.Println("Problem", problemNumber, "is not solved yet.")
+	}
+	if len(os.Args) > 2 {
+		problem.n, _ = strconv.Atoi(os.Args[2])
+	}
+	fmt.Println("Solving problem", problemNumber, "where n :=", problem.n)
+	fmt.Println(problem.solution(problem.n))
 }
