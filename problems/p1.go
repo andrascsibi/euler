0e724a83d4046bddf6fbd8a98c863ffe989661e2
andras <andras.csibi@gmail.com>
2013-09-10 00:48:20 +0200

    hook test

--- problems/p1.go
+++ problems/p1.go
@@ -1,13 +1,11 @@
-package problems 
-
+package problems
 
 func P1(n int) int {
 	sum := 0
 	for i := 1; i < n; i++ {
-		if i % 3 == 0 || i % 5 == 0 {
+		if i%3 == 0 || i%5 == 0 {
 			sum += i
 		}
 	}
- return sum
+	return sum
 }
-
