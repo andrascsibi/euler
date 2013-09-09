e430403ad83a77b14babbe4bff9eb82a32d07b6b
andras <andras.csibi@gmail.com>
2013-09-10 00:40:15 +0200

    solved problem 1

---
+++ problems/p1_test.go
@@ -0,0 +1,21 @@
+package problems
+
+import (
+	"testing"
+)
+
+func TestP1(t *testing.T) {
+
+	var tests = []struct {
+		n, want int
+	}{
+		{10, 23},
+		{1000, 233168},
+		{1000000000, 233168},
+	}
+	for i, c := range tests {
+		if got := P1(c.n); got != c.want {
+			t.Errorf("failed case %v. got %v", i, got)
+		}
+	}
+}
